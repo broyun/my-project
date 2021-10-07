cf42e0aeda9d894300c1e5cf4466c248d57fb11d
broPooh <yhs4705@naver.com>
2021-10-07 20:35:21 +0900

    test222

--- app/src/main/java/com/example/moodimvvmexample/Teste.kt
+++
@@ -1,3 +0,0 @@
-package com.example.moodimvvmexample
-
-data class Teste(val test: String, val name: String)
--- app/src/main/java/com/example/moodimvvmexample/Teste2.kt
+++ app/src/main/java/com/example/moodimvvmexample/Teste2.kt
@@ -1,3 +1,3 @@
 package com.example.moodimvvmexample
 
-data class Teste2(val test: String, val name: String)
+data class Teste2(val test: String, val name: String) //왜 추가했
